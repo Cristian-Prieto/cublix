e3743e3d3800a9dc370e054884e47827a52cfef4
cristian prieto <cristianemanuelprieto@gmail.com>
2022-10-06 18:04:51 +0200

    [id].js

--- pages/Movies/[id].jsx
+++ pages/Movies/[id].jsx
@@ -1,20 +1,29 @@
-import { useRouter } from "next/router";
 import { BASE_URL, API_KEY } from "../../utils/requests";
 import requests from "../../utils/requests";
 
 export default function MovieDetail({ movieDetail }) {
-  const router = useRouter();
-
   return <div>{movieDetail.title}</div>;
 }
 
 export async function getStaticPaths() {
-  const res = await fetch(requests.fetchTrending);
-  const trending = await res.json();
-  const trendingList = trending.results;
+  const res = await Promise.all([
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ]);
+  const allMovieIds = res.map((movies) =>
+    movies.results.map((movie) => movie.id)
+  );
 
-  const paths = trendingList.map((movie) => ({
-    params: { id: movie.id.toString() },
+  const uniqueIds = [...new Set(allMovieIds.flat())];
+
+  const paths = uniqueIds.map((movieId) => ({
+    params: { id: movieId.toString() },
   }));
 
   return { paths, fallback: false };

c8e5a96e71f9ff88882f86541c309501285a21ee
cristian prieto <cristianemanuelprieto@gmail.com>
2022-10-09 19:38:07 +0200

    no te creo

--- components/hooks/useAppContext.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { AppContext } from "../../utils/myList";
-
-export const useAppContext = () => {
-  const { addToMyList } = useContext(AppContext);
-
-  return { addToMyList };
-};
